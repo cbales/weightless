adbc1d544bbb23bfdb6aa612ebbb174274b5b53f
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2017-03-20 12:37:15 -0700

    Start a model

---
+++ database/migrations/2017_03_20_192959_create_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('manufacturer');
+            $table->string('description');
+            $table->double('calories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            Schema::drop('ingredients');
+        });
+    }
+}
