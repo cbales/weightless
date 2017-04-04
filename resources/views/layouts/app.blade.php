aaaf77e8f593c62bf0f2484f5085fdda7e038a6d
Caitlin Bales <c.bales@outlook.com>
2017-04-03 18:08:10 -0700

    Add basic styling

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>@yield('title') - weightless</title>
+		<link href="css/weightless.css" rel="stylesheet" type="text/css">
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+	<div class="container">
+		@section('header')
+			<div class="header">
+				<h1>weightless</h1>
+				<div class="menu">
+					<img src="img/menu.png" alt="Menu" />
+				</div>
+			</div>
+			
+		@show
+
+		<div class="content">
+			@yield('content')
+		</div>
+
+		<div class="footer">
+
+		</div>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('diary');
 });
 
 Route::get('/', 'LogController@show');
