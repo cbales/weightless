693d377bc7c6afa8780f366270acf6a600eb2f90
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2017-04-24 12:37:54 -0700

    Seed dump

--- seed/download.php
+++ seed/download.php
@@ -6,7 +6,11 @@ $curl = curl_init();
 $apiKey = "Rf62TqkVxFqtuOAdPUN7ijLK5KRiTntpnrXHv3jg";
 $totalFiles = ceil(8489/150);
 
-for ($i = 0; $i < 8489; $i += 150) {
+//Update start # to start on a different page of results
+$start = 36*150;
+
+for ($i = $start; $i < 8489; $i += 150) {
+	$pageNo = $i/150+1;
 	curl_setopt_array($curl, array(
 	  CURLOPT_URL => "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=$apiKey&nutrients=205&nutrients=204&nutrients=208&nutrients=269&offset=$i",
 	  CURLOPT_RETURNTRANSFER => true,
@@ -19,12 +23,40 @@ for ($i = 0; $i < 8489; $i += 150) {
 
 	$response = curl_exec($curl);
 
-	file_put_contents("usda".($i/150+1).".json", $response);
-
 	$err = curl_error($curl);
 
 	if ($err) {
 	  echo "cURL Error #:" . $err;
+	  exit();
+	}
+
+	file_put_contents("usda".$pageNo.".json", $response);
+
+	$responses = json_decode($response, true);
+
+	$count = 0;
+	$food = $responses['report']['foods'];
+
+	foreach ($food as $foodItem) {
+		$count++;
+		$id = $foodItem['ndbno'];
+
+		$ch = curl_init();
+		curl_setopt_array($ch, array (
+			CURLOPT_URL => "https://api.nal.usda.gov/ndb/reports/?ndbno=$id&type=b&format=json&api_key=$apiKey",
+			CURLOPT_RETURNTRANSFER => 1
+		));
+
+		$response = curl_exec($ch);
+
+		$err = curl_error($ch);
+
+		if ($err) {
+			echo "cURL Error: " . $err;
+		}
+
+		file_put_contents("measures/" . $pageNo . "-" . $count . ".json", $response);
+		curl_close($ch);
 	}
 
 	echo "Downloaded USDA file " . ($i/150+1) . "/" . $totalFiles . "\n";
